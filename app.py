9ee95915b70981df828471676a72d783908ecf6d
balramsinghraghav1 <balramsinghraghav1@gmail.com>
2025-06-12 08:02:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ def predict():
 
     return render_template('index.html', prediction_text=f"Predicted Price: ${output}k")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
